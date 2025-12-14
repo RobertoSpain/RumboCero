a8986c3dea62d64cf1cebc99190cb3f80b956bf8
Roberto <robertoz1777@gmail.com>
2025-12-15 00:40:05 +0100

    footer landing y contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -7,10 +7,10 @@ const Contacto = () => {
   return (
     <div className="layout-contacto">
       <div className="contenedorcontacto">
-        <h2 className="titulocontacto"> Contacta con Nosotros</h2>
+        <h2 className="titulocontacto">Contacta con Nosotros</h2>
         {esExito ? (
           <div className="mensaje-exito" role="alert" aria-live="polite">
-            <h3>¡Mensaje Enviado! </h3>
+            <h3>¡Mensaje Enviado!</h3>
             <p>Gracias por escribirnos. Te responderemos lo antes posible.</p>
             <a href="/contacto" className="boton-volver">Volver al formulario</a>
           </div>
@@ -20,11 +20,10 @@ const Contacto = () => {
               ¿Tienes dudas o has visto algo raro? Escríbenos y te contestamos volando.
             </p>
 
-            <form 
-              className="formulariocontacto" 
-              action="https://formspree.io/f/meoydzvj" 
-              method="POST"
-            >
+            <form
+              className="formulariocontacto"
+              action="https://formspree.io/f/meoydzvj"
+              method="POST">
               <div className="cajainput">
                 <label htmlFor="nombre">Tu Nombre</label>
                 <input type="text" id="nombre" name="nombre" required aria-required="true" autoComplete="name" />
@@ -44,13 +43,27 @@ const Contacto = () => {
                 <label htmlFor="mensaje">Mensaje</label>
                 <textarea id="mensaje" name="mensaje" rows="5" required aria-required="true" />
               </div>
-              
+
               <button type="submit" className="botonenviar">Enviar Mensaje</button>
               <input type="hidden" name="_next" value={window.location.origin + "/contacto?success=true"} />
             </form>
           </>
         )}
       </div>
+      <footer className="footer-social">
+        <div className="footer-info">
+          Proyecto Realizado por Roberto España | Código en
+          <a href="https://github.com/RobertoSpain/PokeApiJunio" target="_blank" rel="noopener noreferrer">
+            &nbsp;GitHub
+          </a>
+        </div>
+        <a href="https://www.linkedin.com/in/roberto-espa%C3%B1a-540552208/" target="_blank" rel="noopener noreferrer">
+          Linkedin
+        </a>
+        <a href="https://www.facebook.com/roberto.espana.58/?locale=es_ES" target="_blank" rel="noopener noreferrer">
+          Facebook
+        </a>
+      </footer>
     </div>
   );
 };
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,43 +1,44 @@
-import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../assets/Landing.css'; 
 
 export default function Landing() {
-  const navigate = useNavigate();
+    
+    return (
+        <div className="paginalanding">
+            <div className="filtrooscuro"></div>
+            <section className="cajacentral">
+                
+                <h1 className="titulazo">
+                    Viaja mejor acompañado: planifica, organiza y comparte con <span className="resalteverde">Rumbo Cero</span>
+                </h1>
+                <p className="parrafolanding">
+                    Crea itinerarios colaborativos con tus amigos, gestiona listas de equipaje conjuntas y conecta con la comunidad en nuestro foro.
+                    <br />¡Vuestra próxima gran aventura comienza aquí!
+                </p>
 
-  useEffect(() => {
-    const usuarioGuardado = localStorage.getItem('usuario');
-    if (usuarioGuardado) {
-      navigate('/viajes');
-    }
-  }, [navigate]);
-
-  return (
-    <div className="paginalanding">
-      {/* El velo oscuro para que se lea */}
-      <div className="filtrooscuro"></div>
-
-      <div className="cajacentral">
-        
-        <h1 className="titulazo">
-          Viaja mejor acompañado: planifica, organiza y comparte con <span className="resalteverde">Rumbo Cero</span>
-        </h1>
-        
-        <p className="parrafolanding">
-          Crea itinerarios colaborativos con tus amigos, gestiona listas de equipaje conjuntas y conecta con la comunidad en nuestro foro.
-          <br />¡Vuestra próxima gran aventura comienza aquí!
-        </p>
-
-        <div className="zonabotones">
-          <Link to="/login" className="botonlanding botonazul">
-            Comienza a Planificar
-          </Link>
-          
-          <Link to="/registro" className="botonlanding botontransparente">
-            Crear Cuenta
-          </Link>
+                <div className="zonabotones">
+                    <Link to="/login" className="botonlanding botonazul">
+                        Comienza a Planificar
+                    </Link>
+                    <Link to="/registro" className="botonlanding botontransparente">
+                        Crear Cuenta
+                    </Link>
+                </div>
+            </section>
+            <footer className="footer-social">
+                <div className="footer-info">
+                    Proyecto Realizado por Roberto España | Código en 
+                    <a href="https://github.com/RobertoSpain/PokeApiJunio" target="_blank" rel="noopener noreferrer">
+                        &nbsp;GitHub
+                    </a>
+                </div>
+                <a href="https://www.linkedin.com/in/roberto-espa%C3%B1a-540552208/" target="_blank" rel="noopener noreferrer">
+                    Linkedin
+                </a>
+                <a href="https://www.facebook.com/roberto.espana.58/?locale=es_ES" target="_blank" rel="noopener noreferrer">
+                    Facebook
+                </a>
+            </footer>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
