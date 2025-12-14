bc0ed2f93289a11ddd2ce03789f6fd2d8406861b
Roberto <robertoz1777@gmail.com>
2025-12-14 23:02:36 +0100

    probemos asin...

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY, 
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET, 
+  storageBucket: "proyecto-fct-roberto.appspot.com",
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID, 
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
